01c187c2a3a813934898ed973a2e3889625042f9
projectfetcher <calolinalindas2000@gmail.com>
2025-09-03 09:52:43 +0200

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -584,7 +584,7 @@ def scrape_job_details(job_url, licensed=False):
                 logger.info(f'Raw Job Description (length): {len(job_description)}')
                 job_description = re.sub(r'(?i)(?:\s*Show\s+more\s*$|\s*Show\s+less\s*$)', '', job_description, flags=re.MULTILINE).strip()
                 job_description = split_paragraphs(job_description, max_length=200)
-                logger.info(f'Scraped Job Description (length): {len(job_description)}, Paragraphs: {len(job_description.split("\n\n"))}')
+                logger.info(f'Scraped Job Description (length): {len(job_description)}, Paragraphs: {len(job_description.split(delimiter))}')
             else:
                 logger.warning(f"No job description container found for {job_title}")
         else:
